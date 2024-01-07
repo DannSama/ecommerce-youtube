9500f9217ca0da1155d99132d5bb0abd2a5ba02d
Athaya Hanif <96409853+DannSama@users.noreply.github.com>
2024-01-08 02:05:54 +0700

    commit
    ini mau mulai

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -6,7 +6,7 @@
     <meta name="keywords" content="Ogani, unica, creative, html" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Ogani | Template</title>
+    <title>ahha || dashboard </title>
 
     <!-- Google Font -->
     <link
@@ -164,7 +164,7 @@
         <div class="row">
           <div class="col-lg-3">
             <div class="header__logo">
-              <a href="/"><img src="{{ asset('frontend/img/logo.png') }}" alt="" /></a>
+              <a href="/"><img src="{{ asset('frontend/img/logo1.png') }}" alt="" /></a>
             </div>
           </div>
           <div class="col-lg-6">
